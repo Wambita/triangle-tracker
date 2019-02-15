959bec8c99744fc3bcd04bd9815fc05a03e7f3c9
Wambita Fana <wambitafana@gmail.com>
2019-02-15 14:11:06 +0300

    Create a working function

--- js/script.js
+++ js/script.js
@@ -8,13 +8,13 @@ var b = parseInt(bbb);
 var c = parseInt(ccc);
 
 
-//if (a + b <= c) {
-//alert("Not a TRIANGLE!")
-//}else if(a=== b && a=== c && b === c){
-//alert("Equilateral triangle")
-//} else if (a === b || a === c || b===c){
-//alert("Isoceles triangle")
-//} else if (a + b > c){
-// alert("scalene triangle")
+if (a + b <= c) {
+alert("Not a TRIANGLE!")
+}else if(a=== b && a=== c && b === c){
+alert("Equilateral triangle")
+} else if (a === b || a === c || b===c){
+alert("Isoceles triangle")
+} else if (a + b > c){
+ alert("scalene triangle")
 }
 }
