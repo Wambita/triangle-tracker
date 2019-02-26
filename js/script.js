dde59b651782a9656197d835cff38d7f6e26ab8d
Wambita Fana <wambitafana@gmail.com>
2019-02-26 08:27:29 +0300

    Indent js file

--- js/script.js
+++ js/script.js
@@ -1,19 +1,19 @@
-function triangle_tracker(){ 
-    var aaa= document.getElementById ('a').value;
-    var bbb= document.getElementById('b').value;
-    var ccc= document.getElementById('c').value;
-
+function triangle_tracker() {
+    var aaa = document.getElementById('a').value;
+    var bbb = document.getElementById('b').value;
+    var ccc = document.getElementById('c').value;
+  
     var a = parseInt(aaa);
     var b = parseInt(bbb);
     var c = parseInt(ccc);
-
+  
     if (a + b <= c) {
-    alert("Not a TRIANGLE!");
-    }else if(a=== b && a=== c && b === c){
-    alert("Equilateral triangle");
-    } else if (a === b || a === c || b===c){
-    alert("Isoceles triangle");
-    } else if (a!=b && a!=c && b!=c &&  a + b > c){
-    alert("scalene triangle");
+      alert("Not a TRIANGLE!");
+    } else if (a === b && a === c && b === c) {
+      alert("Equilateral triangle");
+    } else if (a === b || a === c || b === c) {
+      alert("Isoceles triangle");
+    } else if (a != b && a != c && b != c && a + b > c) {
+      alert("scalene triangle");
     };
-};
+  };
